6f7d3c6cd31b9e264765e85cd5fe712dd87c0c81
Gabriel Dunienville <gabe.dunienville@gmail.com>
2024-05-29 16:11:54 +0200

    Split to home and etc dirs, update install script, add kmonad udev and systemd'

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+pushd home
 for d in */ ; do
-    echo "${d%/}"
-    stow "${d%/}"
+    echo "stow ${d%/} in $HOME"
+    stow -t $HOME "${d%/}"
 done
+popd
 
+pushd etc
+for d in */ ; do
+    echo "stow ${d%/} in /etc"
+    sudo stow -t /etc "${d%/}"
+done
+popd
