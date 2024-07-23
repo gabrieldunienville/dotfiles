d4a0e724a59f88757c42670ef6e9d8f57acd9b7d
Gabriel Dunienville <gabe.dunienville@gmail.com>
2024-07-23 17:18:36 +0200

    After accidentally deleting neovim config :(

---
+++ home/ipython/.ipython/profile_default/startup/startup.py
@@ -0,0 +1,14 @@
+import builtins
+from IPython.lib.pretty import pprint
+
+
+def custom_print(*args, **kwargs):
+    # Handle empty print call which raise error for ipython pprint
+    if not args and not kwargs:
+        builtins.print()
+        return
+
+    pprint(*args, **kwargs)
+
+
+print = custom_print
