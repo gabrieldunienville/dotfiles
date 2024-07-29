de07da498216be2e15abd34afe338624b8de51a9
Gabriel Dunienville <gabe.dunienville@gmail.com>
2024-07-29 09:40:16 +0200

    Slime: add cell navigation

--- home/nvim/.config/nvim/lua/plugins/slime.lua
+++ home/nvim/.config/nvim/lua/plugins/slime.lua
@@ -1,5 +1,8 @@
 return {
   'jpalardy/vim-slime',
+  dependencies = {
+    'Klafyvel/vim-slime-cells',
+  },
   -- https://github.com/jpalardy/vim-slime/blob/main/assets/doc/advanced.md
   init = function()
     -- these two should be set before the plugin loads
@@ -11,6 +14,7 @@ return {
     vim.g.slime_default_config = { socket_name = 'default', target_pane = ':.2' }
     vim.g.slime_dont_ask_default = true
     vim.g.slime_preserve_curpos = 0
+    vim.g.slime_cell_delimiter = "^#%%"
 
     -- Set this so we can execute a whole code block as one cell in the REPL
     --    Note from docs: If your target supports bracketed-paste, that's a better option than g:slime_python_ipython
@@ -31,5 +35,10 @@ return {
 
     -- Send cell
     vim.keymap.set('n', '<A-r>', '<Plug>SlimeSendCell', { remap = true, silent = false })
+    vim.keymap.set('n', '<A-t>', '<Plug>SlimeCellsSendAndGoToNext', { remap = true, silent = false })
+
+    -- Navigate cells
+    vim.keymap.set('n', '<A-c>', '<Plug>SlimeCellsNext', { remap = true, silent = false })
+    vim.keymap.set('n', '<A-v>', '<Plug>SlimeCellsPrev', { remap = true, silent = false })
   end,
 }
