250f70a4ef74866473541dbb43b8eb078d7e9106
Gabriel Dunienville <gabe.dunienville@gmail.com>
2024-07-29 09:41:51 +0200

    Add mini.operators, mostly for block replace and block duplicate

--- home/nvim/.config/nvim/lua/plugins/mini.lua
+++ home/nvim/.config/nvim/lua/plugins/mini.lua
@@ -30,9 +30,54 @@ return { -- Collection of various small independent plugins/modules
     statusline.section_location = function()
       return '%2l:%-2v'
     end
-    
 
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
+    --
+    require('mini.operators').setup {
+      -- Each entry configures one operator.
+      -- `prefix` defines keys mapped during `setup()`: in Normal mode
+      -- to operate on textobject and line, in Visual - on selection.
+
+      -- Evaluate text and replace with output
+      evaluate = {
+        prefix = 'g=',
+
+        -- Function which does the evaluation
+        func = nil,
+      },
+
+      -- Exchange text regions
+      exchange = {
+        prefix = 'gx',
+
+        -- Whether to reindent new text to match previous indent
+        reindent_linewise = true,
+      },
+
+      -- Multiply (duplicate) text
+      multiply = {
+        prefix = 'gm',
+
+        -- Function which can modify text before multiplying
+        func = nil,
+      },
+
+      -- Replace text with register
+      replace = {
+        prefix = 'gz',
+
+        -- Whether to reindent new text to match previous indent
+        reindent_linewise = true,
+      },
+
+      -- Sort text
+      sort = {
+        prefix = 'gs',
+
+        -- Function which does the sort
+        func = nil,
+      },
+    }
   end,
 }
