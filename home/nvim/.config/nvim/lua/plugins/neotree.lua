d95c145d2dacb65a8f9f29ed2998320e4f6207ce
Gabriel Dunienville <gabe.dunienville@gmail.com>
2024-07-29 09:43:50 +0200

    Neotree: add dir grep

--- home/nvim/.config/nvim/lua/plugins/neotree.lua
+++ home/nvim/.config/nvim/lua/plugins/neotree.lua
@@ -8,10 +8,27 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
+    -- See recipes here for customisation: https://github.com/nvim-neo-tree/neo-tree.nvim/wiki/Recipes
+
     require('neo-tree').setup {
       filesystem = {
         filtered_items = {
           hide_hidden = false,
+        },
+        window = {
+          mappings = {
+            -- ['tf'] = 'telescope_find',
+            -- ['tg'] = 'telescope_grep',
+            [',g'] = function(state)
+              print 'Custom shit'
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              require('telescope.builtin').live_grep {
+                search_dirs = { path },
+                prompt_title = string.format('Grep in [%s]', vim.fs.basename(path)),
+              }
+            end,
+          },
         },
       },
     }
